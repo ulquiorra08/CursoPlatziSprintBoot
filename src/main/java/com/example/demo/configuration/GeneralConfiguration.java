737cf649ae276da4aa3000fae45cb1ce54f16079
Anderson Aponte <andersoncamilo6170@outlook.com>
2023-03-17 16:00:04 -0500

    Configuracion de DataSource en aplicationProperties y paquete configuration

--- src/main/java/com/example/demo/configuration/GeneralConfiguration.java
+++ src/main/java/com/example/demo/configuration/GeneralConfiguration.java
@@ -5,8 +5,11 @@ import com.example.demo.bean.MyBeanWithPropertiesImplements;
 import com.example.demo.pojo.UserPojo;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableConfigurationProperties(UserPojo.class)
@@ -23,6 +26,16 @@ public class GeneralConfiguration {
     @Bean
     public MyBeanWithProperties function(){
         return new MyBeanWithPropertiesImplements(name,apellido);
+    }
+
+    @Bean
+    public DataSource dataSource(){
+        DataSourceBuilder dataSourceBuilder = DataSourceBuilder.create();
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.url("jdbc:h2:men:testdb");
+        dataSourceBuilder.username("sa");
+        dataSourceBuilder.password("");
+        return dataSourceBuilder.build();
     }
 
 }
